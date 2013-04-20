83fd738639cb8cef4dc6d342ac4184a31b0595d4
Alex Savin <karismafilms@gmail.com>
2013-04-20 17:24:05 +0300

    Voyager pics added, planets exists and almost aligned, years are ticking

--- hello3d/js/main.js
+++ hello3d/js/main.js
@@ -1,1 +1,25 @@
+var createPlanets = function(scene) {
+	var planet2 = new THREE.Mesh(
+	        new THREE.SphereGeometry(10, 10, 10),
+	        new THREE.MeshLambertMaterial({color: 0x00B3FF}));
+	
+	
+    scene.add(planet2);
+	
+	var planetMercury = new THREE.Mesh(
+	        new THREE.SphereGeometry(10, 10, 10),
+	        new THREE.MeshLambertMaterial({color: 0xE9F235}));
+			
+	scene.add(planetMercury);
+	
+	var planetVenus = new THREE.Mesh(
+	        new THREE.SphereGeometry(10, 10, 10),
+	        new THREE.MeshLambertMaterial({color: 0xF2B035}));
+			
+	scene.add(planetVenus);
+}
 
+$('#mylink').click(function(e){
+	e.preventDefault();
+	console.log('clicked!');
+});
